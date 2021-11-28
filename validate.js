47880c4590641f607e4d46209417ab0a636eda42
Thushad-T <thushadt45@gamil.com>
2021-11-28 22:54:04 +0530

    first commit

---
+++ validate.js
@@ -0,0 +1,75 @@
+var username = document.getElementById("name")
+var email = document.getElementById("email")
+var number = document.getElementById("number")
+var messege = document.getElementById("messege")
+var err = document.getElementsByClassName("err")
+
+var letter = /^[a-zA-Z]+$/
+var flag = 0
+var numletter = /^[0-9]+$/
+
+
+
+
+
+$("#submit-form").submit((e)=>{
+    e.preventDefault()
+    flag = 0
+    
+     checknumber()
+     checkphoneNumber()
+     console.log(flag)
+
+     if(flag == 2){
+        $.ajax({
+            url:"https://script.google.com/macros/s/AKfycbywsbCugC8028mvxyO_XEhWOqR7KZko0guGItX4kA/exec",
+            data:$("#submit-form").serialize(),
+            method:"post",
+            success:function (response){
+                alert("Form submitted successfully")
+                window.location.reload()
+                //window.location.href="https://google.com"
+            },
+            error:function (err){
+                alert("Something Error")
+    
+            }
+        })
+     }
+
+})
+
+function checknumber(){
+
+    // console.log(username.value)
+    if(username.value == ""){
+        err[0].innerHTML = "null not alloweded"
+    }else if(username.value.trim() == false){
+        err[0].innerHTML = "space not allowedde"
+    }else if(letter.test(username.value) == false){
+        err[0].innerHTML = "only character are allowded"
+    }else{
+        err[0].innerHTML = ""
+        flag += 1
+        // console.log(flag)
+    }
+}
+
+function checkphoneNumber(){
+    if(number.value == ""){
+        err[2].innerHTML = "null not allowed"
+    }
+    else if(number.value.trim() ==""){
+        err[2].innerHTML = "space not allowed"
+    }
+    else if(numletter.test(number.value) == false){
+     err[2].innerHTML = "only numbers are allowed"
+    }
+    else if( number.value.length > 10){
+            err[2].innerHTML = "only 10 numbers"
+    }
+    else{
+        err[2].innerHTML = ""
+        flag+=1;
+    }
+}
